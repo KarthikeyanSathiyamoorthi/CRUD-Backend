b2721088394210b309ebee42abc4ed318b46039c
Karthikeyan <tamilkn23@gmail.com>
2026-01-11 21:04:52 +0530

    Added the user authentication routes

---
+++ models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+// Define the structure of the User Date
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+});
+
+// Create and export the Model
+const User = mongoose.model("User", userSchema);
+module.exports = User;
