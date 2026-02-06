a091de00089d23d95c2ad09a0f3d198f2314f61b
Karthikeyan <tamilkn23@gmail.com>
2026-02-06 15:19:20 +0530

    feat: configure environment files and update scripts for deployment

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:3000", // Your React app URL
+    origin: process.env.FRONTEND_URL || "http://localhost:3000", // Your React app URL
     credentials: true, // Allow cookies to be sent
   }),
 );
