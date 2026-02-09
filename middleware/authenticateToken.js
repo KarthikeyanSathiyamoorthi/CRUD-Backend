35ef3a281f065b683bb53dccb0adb60c464ca7d9
Karthikeyan <tamilkn23@gmail.com>
2026-02-09 13:37:22 +0530

    refactor: Remove the console.logs

--- middleware/authenticateToken.js
+++ middleware/authenticateToken.js
@@ -2,8 +2,6 @@ const jwt = require("jsonwebtoken");
 
 const authenticateToken = (req, res, next) => {
   const accessToken = req.cookies.accessToken;
-  console.log("Inside authenticateToken req.cookies: ", req.cookies);
-  console.log("Inside authenticateToken Headers: ", req.headers.cookie);
 
   if (!accessToken)
     return res.status(401).json({ message: "Access token is missing" });
